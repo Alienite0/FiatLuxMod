ec0320322c881e086a79162f77b995a2263aebbc
Alienite <alieniteyt@gmail.com>
2025-05-17 13:29:28 -0400

    First commit

---
+++ common/00_fiatlux.lua
@@ -0,0 +1,10 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+	NDefines.NGame.END_DATE = "1850.1.10"
+	NDefines.NCountry.MAX_CROWN_COLONIES = 6
+	NDefines.NAI.AGGRESSIVENESS = 850.0
+	NDefines.NAI.AGGRESSIVENESS_BONUS_EASY_WAR = 800.0
+	
+	NDefines.NReligion.ORTHODOX_ICON_AUTHORITY_COST = 0.20
